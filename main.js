c6e2ccf5da5ff9861a52eb291ab88beffd435afc
Manil <53854029+Mohamed-Manil@users.noreply.github.com>
2021-10-20 15:02:46 +0100

    created a preload file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -6,6 +7,7 @@ function createWindow() {
     width: 800,
     webPreferences: {
       nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
