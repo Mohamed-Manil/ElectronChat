f2cc7d429c889b1ae7affd6bf33de837b85ed54d
Manil <53854029+Mohamed-Manil@users.noreply.github.com>
2021-10-30 16:45:53 +0100

    did some refactoring and component spliting .

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navitem from "./comps/NavItem";
+import LogoItem from "./comps/LogoItem";
 
 const Sidebar = () => {
   return (
     <div className="flex flex-row h-full">
       <nav className="bg-blue-dark dark:bg-gray-darkest w-20 h-screen flex flex-col">
-        <Navitem idItem="logo" />
+        <LogoItem idItem="logo" />
         <hr className="w-1/2 mx-auto border-gray-700 mt-2 mb-2" />
-        <Navitem idItem="logo2" typeItem={"1"} />
+        <Navitem idItem="logo2" />
       </nav>
     </div>
   );
--- src/components/sidebar/comps/LogoItem.js
+++ src/components/sidebar/comps/LogoItem.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const Item = ({ idItem } = {}) => {
-  const dummyImageUrl = "https://via.placeholder.com/150";
+const LogoItem = ({ idItem }) => {
   return (
     <>
       <input
@@ -13,15 +12,16 @@ const Item = ({ idItem } = {}) => {
       <label htmlFor={idItem}>
         <div className="flex relative flex-col h-full">
           <div className="absolute left-0 top-1/2 w-1 h-4/5 bg-white rounded-r-lg transform -translate-y-1/2"></div>
-          <img
-            src={dummyImageUrl}
-            alt="itm"
-            className="my-auto mx-auto w-2/3 rounded-full"
-          />
+          <div
+            className="my-auto mx-auto text-white font-logoFont bg-blue-500 rounded-full flex justify-center items-center"
+            style={{ width: "3.33333rem", height: "3.33333rem" }}
+          >
+            <div className="text-2xl">Ce</div>
+          </div>
         </div>
       </label>
     </>
   );
 };
 
-export default Item;
+export default LogoItem;
